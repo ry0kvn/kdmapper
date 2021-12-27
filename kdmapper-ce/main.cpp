99f6710f4e3bd8d618259dab2b054c97272724c5
ry0kvn <sei.katu.ryou@gmail.com>
2021-12-28 08:32:35 +0900

    add todo

--- kdmapper-ce/main.cpp
+++ kdmapper-ce/main.cpp
@@ -2,6 +2,12 @@
 #include "kdmapper-ce.hpp"
 
 int wmain(const int argc, wchar_t** argv) {
-	printf("hello\n");
+	// TODO: コマンドライン引数に与えられたファイルをメモリにロード
+	// TODO: dbk64ファイルをディスクにドロップ
+	// TODO: サービスの作成，開始
+	// TODO: ドロップしたdbk64ファイルを削除
+	// TODO: kernelmoduleunloaderにシェルコードをインジェクトし，dbk64サービスのハンドルを取得
+	// TODO: DeviceIoControlを使いドライバをロード
+	// TODO: サービスの停止，削除
 	return 0;
 }
